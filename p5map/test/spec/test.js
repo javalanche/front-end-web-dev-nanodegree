e9a5b765d7140a8c2dfc83cabd8695ad575070cf
Javier Fraga <javier.s.fraga@gmail.com>
2015-05-10 09:18:33 -0500

    changed p5map to yo webapp

---
+++ p5map/test/spec/test.js
@@ -0,0 +1,12 @@
+/* global describe, it */
+
+(function () {
+  'use strict';
+
+  describe('Project 5 FEWD ND Udactiy Neighborhood Map Project', function () {
+    describe('when page loads', function () {
+      it('should display "Neighborhood Map Project"in the header', function () {
+      });
+    });
+  });
+})();
